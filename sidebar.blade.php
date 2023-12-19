7de3dda39770d05b7c3f517726cde165da34b3a1
belasriiimad <imadbelasri@gmail.com>
2023-12-19 08:57:35 +0000

    first commit

---
+++ sidebar.blade.php
@@ -0,0 +1,12 @@
+<nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar">
+    <div class="position-sticky pt-3">
+        <ul class="nav flex-column">
+            <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="#">
+                    <i class="fas fa-dashboard"></i>
+                    Dashboard
+                </a>
+            </li>
+        </ul>
+    </div>
+</nav>
